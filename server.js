fdfd90f7f636426a9f7ef0f6aa90b511f77fcb98
mikebaradaran <mike.baradaran@qa.com>
2023-08-02 23:31:32 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+let data = [];
+const PORT = 4000; // You can change this to any available port
+
+// POST endpoint to receive data
+app.post('/api/data', (req, res) => {
+    const newData = req.body;
+    data.push(newData);
+    res.status(201).json({ message: 'Data received successfully', data: newData });
+});
+
+// GET endpoint to retrieve all data
+app.get('/api/get', (req, res) => {
+    res.json({ data });
+});
+
+app.post('/api/kill', (req, res) => {
+    data.length = 0;
+    res.status(201).json({ message: 'All Data cleared'});
+});
+
+app.post('/api/wipe', (req, res) => {
+    data.forEach(element => {
+        element.message = "";
+    });
+    res.status(201).json({ message: 'All messages wiped'});
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
